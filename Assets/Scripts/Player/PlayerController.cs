78972a6b0bfb24cb939a35e8d505a2c885b66d66
eldreco <andreffcd@gmail.com>
2023-05-09 22:32:32 +0100

    Fixed sliding on ice feeling

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,8 +11,14 @@ public class PlayerController : MonoBehaviour
     private PlayerInput playerInput;
     private PlayerInputActions playerInputActions;
 
+    [SerializeField] private bool onAir = false;
+    private bool movingRight = false;
+    private bool movingLeft = false;
+    private bool isColliding = false;
+
     [SerializeField] private float jumpHeight = 5f;
-    [SerializeField] private float moveSpeed = 3f;
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float gravityValue = 500f;
 
 
     private void Awake() {
@@ -26,22 +32,39 @@ public class PlayerController : MonoBehaviour
         playerInputActions.Player.Movement.performed += Move;
     }
 
+    private void OnCollisionEnter2D(Collision2D other) {
+        isColliding = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        isColliding = false;
+    }
+
     private void FixedUpdate() {
-        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
-        rb.AddForce(inputVector * moveSpeed, ForceMode2D.Force);
+        if(!(onAir && isColliding)){
+            Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
+            rb.velocity = new Vector2(inputVector.x * moveSpeed, rb.velocity.y);
+        }
+        ApplyGravity();
+    }
+
+    private void ApplyGravity(){
+        onAir = rb.velocity.y != 0;
+        if(onAir){
+            rb.AddForce(new Vector2(0, -1f * gravityValue * Time.deltaTime), ForceMode2D.Force);
+        }
     }
 
     private void Move(InputAction.CallbackContext context)
     {
         Vector2 inputVector = context.ReadValue<Vector2>();
-        Debug.Log(inputVector);
-        rb.AddForce(inputVector * moveSpeed, ForceMode2D.Force);
+        rb.velocity = new Vector2(inputVector.x * moveSpeed, rb.velocity.y);
     }
 
     public void Jump(InputAction.CallbackContext context){
-        if(context.performed){
-            Debug.Log("JUMP");
+        if(context.performed && rb.velocity.y == 0){
             rb.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+            onAir = true;
         }
     }
 }
