359cc2ffa68499adbf9ebf1f5f0465fabdcb083b
STRIKENSUN41 <matthew.wolfe4001@gmail.com>
2023-05-16 16:47:05 -0500

    Now the float sink system works

--- Assets/Scripts/Environment/Water.cs
+++ Assets/Scripts/Environment/Water.cs
@@ -11,6 +11,7 @@ public class Water : MonoBehaviour
     private bool isVertical = false;
     private bool inWater = false;
 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +19,24 @@ public class Water : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void OnTriggerStay2D(Collider2D collision) 
     {
-        
+        //Debug.Log("Here Dog!");
+        if (collision.transform.tag == "Player" && !isVertical && !inWater)
+        {
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 0.5f), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) 
+    {
+        if (collision.transform.tag == "Player") 
+        {
+            bCollider.enabled = true;
+            player.setInWater(false);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -29,21 +44,25 @@ public class Water : MonoBehaviour
         isVertical = player.getVertical();
         inWater = player.getInWater();
 
+
         if (collision.transform.tag == "Player" && !inWater)
         {
-            if (collision.transform.tag == "Player" && !isVertical)
+
+            if (collision.transform.tag == "Player" && !isVertical) //Single-File
             {
-                Debug.Log("Is NOT Vertical");
-                bCollider.isTrigger = false;
-                player.setInWater(false);
-                player.setJumpHeight(12f);
+                bCollider.enabled = true;
+                //Debug.Log("Is NOT Vertical");
+                //bCollider.isTrigger = false;
+                //player.setInWater(false);
+                //player.setJumpHeight(12f);
             }
-            else if (collision.transform.tag == "Player" && isVertical)
+            else if (collision.transform.tag == "Player" && isVertical) //Stacked
             {
-                Debug.Log("Is Vertical");
+                bCollider.enabled = false;
+                //Debug.Log("Is Vertical");
                 bCollider.isTrigger = true;
                 player.setInWater(true);
-                player.setJumpHeight(15f);
+                //player.setJumpHeight(15f);
             }
         }
     }
