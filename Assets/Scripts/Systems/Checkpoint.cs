7dd6916ff6cd75fec87accb0d99eee5db0571c9b
STRIKENSUN41 <matthew.wolfe4001@gmail.com>
2023-05-16 18:23:02 -0500

    Added flag animations

--- Assets/Scripts/Systems/Checkpoint.cs
+++ Assets/Scripts/Systems/Checkpoint.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
@@ -7,28 +8,26 @@ public class Checkpoint : MonoBehaviour
     [SerializeField]
     private Sprite sprite;
     
-    private SpriteRenderer sr;
+    //private SpriteRenderer sr;
+    private Animator anim;
 
     private Respawn respawnZone;
 
     // Start is called before the first frame update
     void Start()
     {
-        sr = transform.GetComponent<SpriteRenderer>();
+        //sr = transform.GetComponent<SpriteRenderer>();
+        anim = transform.GetComponent<Animator>();
         respawnZone = GameObject.FindGameObjectWithTag("Respawn").GetComponent<Respawn>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
-            sr.sprite = sprite;
+            //sr.sprite = sprite;
+            anim.enabled = true;
             respawnZone.storeCheckpointData(this.transform.position);
             respawnZone.setCheckpointBool(true);
         }
