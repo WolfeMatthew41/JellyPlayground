cf7a1d9127e96af32703bad2cd365f0f3daa7fd7
eldreco <andreffcd@gmail.com>
2023-05-17 01:46:43 +0100

    Finished Lost_Jelly

--- Assets/Scripts/Systems/Collectable.cs
+++ Assets/Scripts/Systems/Collectable.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,19 +11,13 @@ public class Collectable : MonoBehaviour
 
     private TMP_Text coinsText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
             DataManager.GetInstance().AddCoinValue(1);
             UIManager.GetInstance().UpdateUI();
-            Destroy(gameObject);
+            gameObject.GetComponent<Animator>().Play("LostJellyHappy");
         }
     }
 }
